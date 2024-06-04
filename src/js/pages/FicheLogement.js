1c0d775ab07d407eb1897db083a655c2a7c81f88
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-06-04 04:41:16 +0200

    creation de la 4e page ( Fiche Logement )

---
+++ src/js/pages/FicheLogement.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import logements from '../../data/logements.json';
+import '../../css/FicheLogement.css';
+
+function FicheLogement() {
+    
+}
+
+export default FicheLogement;
