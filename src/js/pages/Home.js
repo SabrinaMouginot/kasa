7eccb9532906bf6024af0011290396ba2d1538a8
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-06-05 10:45:48 +0200

    travail de style sur les cartes

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -3,30 +3,33 @@ import { Link } from 'react-router-dom';
 import logements from '../../data/logements.json';
 import '../../css/Home.css';
 
-// // const Home = () => {
-// //   return (
-// //     <div>
-// //       <h1>Welcome to Kasa</h1>
-// //       <p>This is the Home page.</p>
-// //     </div>
-// //   );
-// // };
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to Kasa</h1>
+//       <p>This is the Home page.</p>
+//     </div>
+//   );
+// };
+
 
 function Home() {
   const displayedLogements = logements.slice(0, 6); // Limite l'affichage à 6 cartes
 
   return (
     <div className="home">
-      <h1>Liste des logements</h1>
-      <div className="logements-list">
-        {displayedLogements.map(logement => (
-          <div key={logement.id} className="logement-card">
-            <Link to={`/logement/${logement.id}`}>
-              <img src={logement.cover} alt={logement.title} />
-              <h2>{logement.title}</h2>
-            </Link>
-          </div>
-        ))}
+      <h1>Chez vous, partout et ailleurs</h1> {/* Titre en dehors de la boîte grise */}
+      <div className="logements-box"> {/* Boîte grise contenant les cartes */}
+        <div className="logements-list">
+          {displayedLogements.map(logement => (
+            <div key={logement.id} className="logement-card">
+              <Link to={`/logement/${logement.id}`}>
+                <img src={logement.cover} alt={logement.title} />
+                <h2>{logement.title}</h2>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
