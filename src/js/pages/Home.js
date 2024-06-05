4b336872d76eb4e7a0a17418e7726c0508561056
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-06-05 10:25:34 +0200

    Home : creation des cartes

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,28 +1,25 @@
-// import React from 'react';
-// import '../../css/Home.css'; 
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <h1>Welcome to Kasa</h1>
-//       <p>This is the Home page.</p>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logements from '../../data/logements.json';
 import '../../css/Home.css';
 
+// // const Home = () => {
+// //   return (
+// //     <div>
+// //       <h1>Welcome to Kasa</h1>
+// //       <p>This is the Home page.</p>
+// //     </div>
+// //   );
+// // };
+
 function Home() {
+  const displayedLogements = logements.slice(0, 6); // Limite l'affichage à 6 cartes
+
   return (
     <div className="home">
       <h1>Liste des logements</h1>
       <div className="logements-list">
-        {logements.map(logement => (
+        {displayedLogements.map(logement => (
           <div key={logement.id} className="logement-card">
             <Link to={`/logement/${logement.id}`}>
               <img src={logement.cover} alt={logement.title} />
