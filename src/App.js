85f77a55f814c05a57511814966145a5cf7a705c
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-06-05 12:11:43 +0200

    nettoyage et rectification sur le nb de cartes affiche ( pas limite a 6 )

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './js/components/Header';
@@ -42,12 +15,12 @@ function App() {
     <BrowserRouter>
       <Header />
       <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/logement/:id" element={<FicheLogement />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/logement/:id" element={<FicheLogement />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -14,14 +14,14 @@ import '../../css/Home.css';
 
 
 function Home() {
-  const displayedLogements = logements.slice(0, 6); // Limite l'affichage à 6 cartes
+  // const displayedLogements = logements.slice(0, 6); // Limite l'affichage à 6 cartes
 
   return (
     <div className="home">
       <h1>Chez vous, partout et ailleurs</h1> {/* Titre en dehors de la boîte grise */}
       <div className="logements-box"> {/* Boîte grise contenant les cartes */}
         <div className="logements-list">
-          {displayedLogements.map(logement => (
+          {logements.map(logement => (
             <div key={logement.id} className="logement-card">
               <Link to={`/logement/${logement.id}`}>
                 <img src={logement.cover} alt={logement.title} />
