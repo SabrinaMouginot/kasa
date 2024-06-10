3892ffa06c6c4bdf9e57fd3b6aa5cb50dc67be0d
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-06-10 03:51:38 +0200

    travail sur la banniere : titre et img de couverture

--- src/js/components/Banner.jsx
+++ src/js/components/Banner.jsx
@@ -5,7 +5,6 @@ import '../../css/Banner.css';
 function Banner() {
     return (
         <article className="banner">
-            {/* <h1>Chez vous, partout et ailleurs</h1> */}
         </article>
     )
 }
