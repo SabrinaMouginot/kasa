4f1af9a26f940eaee00ebe10e26e1dbfc46accdb
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-01 10:18:40 +0200

    changement des etoiles de la note ( forme et couleur ) . Les etoiles ne sont pas cliquable mais sont une note stockée dans le fichier json

--- src/js/pages/FicheLogement.jsx
+++ src/js/pages/FicheLogement.jsx
@@ -9,14 +9,13 @@ import Dropdown from '../components/Dropdown';
 function FicheLogement() {
   const { id } = useParams();
   const [logement, setLogement] = useState(null);
-  const [rating, setRating] = useState(0);
 
   useEffect(() => {
     fetch('/logements.json')
       .then(response => response.json())
       .then(data => {
         const foundLogement = data.find(logement => logement.id === id);
-        if(foundLogement){
+        if (foundLogement) {
           setLogement(foundLogement);
         }
       })
@@ -26,6 +25,12 @@ function FicheLogement() {
   if (!logement) {
     return <NotFound />;
   }
+
+  const StarIcon = ({ filled }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+      <path d="M19.7212 6.84366C19.4728 6.32809 18.9479 6 18.3714 6C17.7949 6 17.2747 6.32809 17.0216 6.84366L14.0079 13.0445L7.27748 14.0382C6.71506 14.1225 6.24636 14.5162 6.07295 15.0552C5.89953 15.5942 6.04014 16.1895 6.44321 16.5879L11.327 21.4201L10.174 28.2491C10.0803 28.8115 10.3146 29.3833 10.7786 29.7161C11.2426 30.0489 11.8566 30.0911 12.3628 29.8239L18.3761 26.6133L24.3894 29.8239C24.8956 30.0911 25.5096 30.0536 25.9736 29.7161C26.4376 29.3786 26.6719 28.8115 26.5782 28.2491L25.4205 21.4201L30.3043 16.5879C30.7073 16.1895 30.8526 15.5942 30.6745 15.0552C30.4964 14.5162 30.0324 14.1225 29.47 14.0382L22.7349 13.0445L19.7212 6.84366Z" fill={filled ? "#FF6060" : "#E3E3E3"} />
+    </svg>
+  );
 
   return (
     <div className="fiche-logement">
@@ -40,16 +45,8 @@ function FicheLogement() {
 
       <h2>{logement.location}</h2>
 
-      {/* <div>
-        {logement.tags.map(tag => (
-          <span key={index} className="tag">{tag}</span>
-        ))
-        }
-      </div> */}
       <div className="rating-tags">
         <div className="tags-list">
-          {/* {logement.tags.map((tag, index) => (
-          <Tag key={index} tag={tag} /> */}
           {logement.tags.map(tag => (
             <Tag key={`${logement.id}-${tag}`} tag={tag} />
           ))}
@@ -58,22 +55,19 @@ function FicheLogement() {
         <div className="title-owner">
           <div className="rating">
             {[...Array(5)].map((_, index) => (
-              <span
-                key={index}
-                className={`star ${index < rating ? 'filled' : ''}`}
-                onClick={() => setRating(index + 1)}
-              >
-                ★
+              <span key={index} className="star">
+                <StarIcon filled={index < logement.rating} />
               </span>
             ))}
+          </div>
 
-            <div className="owner-info mobile">
-              <span className="owner-name">{logement.host.name}</span>
-              <img src={logement.host.picture} alt={logement.host.name} className="owner-picture" />
-            </div>
+          <div className="owner-info mobile">
+            <span className="owner-name">{logement.host.name}</span>
+            <img src={logement.host.picture} alt={logement.host.name} className="owner-picture" />
           </div>
         </div>
       </div>
+
       <div className="dropdowns">
         <Dropdown title="Description" content={logement.description} />
         <Dropdown title="Equipements" content={logement.equipments.join(', ')} />
