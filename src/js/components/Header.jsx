57aa5e7df013c3b5e9741a4aa1d884df795fe9b2
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-10 16:00:03 +0200

    page fiche logement - style fleche du carrousel

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import '../../css/Header.css'; 
-
-// function Header() {
-//   return (
-//     <header className="header">
-//       {/* <img src={logo} alt="Logo" className="logo" /> */}
-//       <img src={`/assets/LOGOHEADER.png`} alt="Kasa Logo" className="header-logo" />
-//       <nav>
-//         <ul className="nav-list">
-//           <li className="nav-item">
-//             <NavLink to="/" activeClassName="active-link" exact>Accueil</NavLink>
-//           </li>
-//           <li className="nav-item">
-//             <NavLink to="/about" activeClassName="active-link">À propos</NavLink>
-//           </li>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// }
-
-// export default Header;
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../css/Header.css'; 
@@ -34,7 +9,7 @@ function Header() {
       <nav>
         <ul className="nav-list">
           <li className="nav-item">
-            <NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>Accueil</NavLink>
+            <NavLink to="/"  className={({ isActive }) => (isActive ? 'active-link' : '')}>Accueil</NavLink>
           </li>
           <li className="nav-item">
             <NavLink to="/about" className={({ isActive }) => (isActive ? 'active-link' : '')}>À propos</NavLink>
